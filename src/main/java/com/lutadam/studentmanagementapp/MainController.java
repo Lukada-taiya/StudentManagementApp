b60095ea78750911fc08bbea6cfb25db3fa0e175
Lukada-taiya <lukmanadam43@gmail.com>
2022-10-05 14:10:38 +0000

    Added unfinished event handler for login button

--- src/main/java/com/lutadam/studentmanagementapp/MainController.java
+++ src/main/java/com/lutadam/studentmanagementapp/MainController.java
@@ -1,6 +1,7 @@
 package com.lutadam.studentmanagementapp;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
@@ -27,4 +28,22 @@ public class MainController {
     public void close() {
         System.exit(0);
     }
+
+    @FXML
+    public void btnLoginAction() {
+        Alert alert;
+        String name = tfUsername.getText().trim();
+        String password = tfPassword.getText().trim();
+
+        if(name.isEmpty() || password.isEmpty()) {
+            alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Login Error");
+            alert.setHeaderText(null);
+            alert.setContentText("All blank fields must be filled");
+            alert.show();
+        }else {
+            //Start db test
+        }
+    }
+
 }
